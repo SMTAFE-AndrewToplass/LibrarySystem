a264f8717af7cf163c928f087fc3395febfb6bbf
SMTAFE-AndrewToplass <160808664+SMTAFE-AndrewToplass@users.noreply.github.com>
2024-09-19 11:51:27 +0800

    add documentation comment

--- LibrarySystem/Book.cs
+++ LibrarySystem/Book.cs
@@ -15,17 +15,28 @@ namespace LibrarySystem
         public int UserId { get; private set; }
         public DateOnly? DueDate { get; private set; }
 
+        /// <summary>
+        /// Returns a bool to state whether the book is available, based on if the UserId is -1.
+        /// </summary>
         public bool IsAvailable
         {
             get => UserId == -1;
         }
 
+        /// <summary>
+        /// Assign a User to a Book, will set the Book's UserId and DueDate properties.
+        /// </summary>
+        /// <param name="userId">The userId to assign the Book to.</param>
+        /// <param name="dueDate">The new due date, when the book should be returned.</param>
         public void AssignUser(int userId, DateOnly dueDate)
         {
             UserId = userId;
             DueDate = dueDate;
         }
 
+        /// <summary>
+        /// Removes the User information from this book, should be called when returning Book.
+        /// </summary>
         public void RemoveUser()
         {
             UserId = -1;
--- LibrarySystem/User.cs
+++ LibrarySystem/User.cs
@@ -8,8 +8,15 @@ namespace LibrarySystem
 {
     internal class User(int userId, string name, string email, double feesOwed = 0)
     {
-        protected const int maxNumberOfBooks = 3;
-        protected const int borrowDays = 5;
+        /// <summary>
+        /// How many books can a User borrow at once.
+        /// </summary>
+        public const int MaxNumberOfBooks = 3;
+
+        /// <summary>
+        /// How many days a Book can be borrowed for before late fees.
+        /// </summary>
+        public const int BorrowDays = 5;
 
         public int UserId { get; } = userId;
         public string Name { get; } = name;
@@ -17,25 +24,42 @@ namespace LibrarySystem
         public List<Book> Books { get; } = [];
         public double FeesOwed { get; private set; } = feesOwed;
 
+        /// <summary>
+        /// Borrow a list of Books, up to User.maxNumberOfBooks at a time.
+        /// </summary>
+        /// <param name="books">A list of Books to borrow.</param>
+        /// <exception cref="Exception">Thrown if User did not return their Books or did not pay their fees before borrowing again.</exception>
+        /// <exception cref="ArgumentException">Thrown if there are too many Books at once or a Book is not available.</exception>
         public void BorrowBooks(IEnumerable<Book> books)
         {
+            if (FeesOwed > 0.0)
+            {
+                // User must pay any fees they owe before borrwing again.
+                throw new Exception($"User {Name} must pay their late fees before borrowing again.");
+            }
+
             if (Books.Count > 0)
             {
+                // User must return their existing Books before borrowing again.
                 throw new Exception($"User {Name} must return their borrowed books before borrowing again.");
             }
 
-            // Get todays date for 
-            DateOnly today = DateOnly.FromDateTime(DateTime.Now);
-            if (books.Count() > maxNumberOfBooks)
+            // Get todays date to calculate new due date.
+            DateOnly today = DateOnly.FromDateTime(DateTime.Today);
+            if (books.Count() > MaxNumberOfBooks)
             {
-                throw new ArgumentException($"You can only borrow {maxNumberOfBooks} books at a time.");
+                throw new ArgumentException($"You can only borrow {MaxNumberOfBooks} book(s) at a time.");
             }
 
             foreach (Book book in books)
             {
+                // Check if the Book is available before borrowing it.
                 if (book.IsAvailable)
                 {
-                    DateOnly dueDate = today.AddDays(borrowDays);
+                    // Set the due date to today + how many days the Book can be borrowed for.
+                    DateOnly dueDate = today.AddDays(BorrowDays);
+
+                    // Add the User's information to the Book, then add it to the User's Book list.
                     book.AssignUser(UserId, dueDate);
                     Books.Add(book);
                 }
@@ -46,24 +70,28 @@ namespace LibrarySystem
             }
         }
 
-        public void ReturnBooks(IEnumerable<Book> books)
+        /// <summary>
+        /// Return all books the user is currently borrowing.
+        /// </summary>
+        public void ReturnBooks()
         {
-            DateOnly today = DateOnly.FromDateTime(DateTime.Now);
-            foreach (Book book in books)
+            DateOnly today = DateOnly.FromDateTime(DateTime.Today);
+            foreach (Book book in Books)
             {
                 if (book.UserId == UserId && book.DueDate is not null)
                 {
                     DateOnly dueDate = (DateOnly)book.DueDate;
+
+                    // Get the number of days between today and the book's due date, limited to 0 if the due date is in the future.
                     int daysLate = Math.Max(today.DayNumber - dueDate.DayNumber, 0);
-                    double fees = Library.lateFeePerDay * daysLate;
+
+                    // Apply late fees for returning the Book late (no fees are applied if daysLate is 0).
+                    double fees = Library.LateFeePerDay * daysLate;
                     FeesOwed += fees;
 
+                    // Remove the User information from the Book and from the User's Book list.
                     book.RemoveUser();
                     Books.Remove(book);
-                }
-                else
-                {
-                    throw new ArgumentException($"User {Name} has not borrowed book {book.Title}.");
                 }
             }
         }
